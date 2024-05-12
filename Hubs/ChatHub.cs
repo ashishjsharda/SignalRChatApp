73e019268af9c4941e2fbdc2d74286e112db3003
Ashish Sharda <ashishjsharda@gmail.com>
2024-05-12 18:18:31 -0400

    Add new Hub

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Hubs
+{
+    public class ChatHub :Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("Receive Message", user, message);
+        }
+    
+    }
+}
+
+
